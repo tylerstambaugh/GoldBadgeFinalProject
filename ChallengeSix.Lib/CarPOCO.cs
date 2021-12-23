b8a078e3fef44d51e71d5defc5c87ba36580b90a
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2021-12-22 21:12:48 -0500

    working on challenge 6

---
+++ ChallengeSix.Lib/CarPOCO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeSix.Lib
+{
+    public abstract class Car
+    {
+        public int Id { get; set; }
+        protected string Make { get; set; }
+        protected string Model { get; set; }
+        protected int Year { get; set; }
+        protected double EfficiencyRating { get; set; }
+
+
+    }
+
+    public class GasVehicle : Car
+    {
+        protected double engineDisplacement { get; set; }
+        protected double MilesPerGallon { get; set; }
+
+    }
+
+    public class ElectricVehicle : Car
+    {
+        protected double batterySize { get; set; }
+        protected TimeSpan ChargeTime { get; set; }
+        protected int RangeInMiles { get; set; }
+
+    }
+
+    public class Hybrid : GasVehicle 
+    {
+        protected string FuelCell { get; set; }
+    }
+
+}
---
+++ ChallengeSix.Lib/CarRepo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeSix.Lib
+{
+    class CarRepo
+    {
+        //CRUD operations on local list of cars.
+
+        //local list of cars to do business logic with
+        private readonly List<Car> _CarRepo = new List<Car>();
+
+        //creat (add) a car to the list
+        public bool AddCarToList(Car c)
+        {
+            if (c != null)
+            {
+                _CarRepo.Add(c);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //update a car on the list
+
+        public bool UpdateCar(int carId)
+        {
+            foreach (Car c in _CarRepo)
+            {
+                if (c.Id == carId)
+                {
+                    //update the car details.
+                    return true;
+
+                }
+
+                else
+                {
+                    return false;
+                }
+                
+            }
+            return false;
+        }
+
+        //read out cars from the list
+
+        //delete cars from the list
+
+        // helper methods
+    }
+}
